495c1d18917c83c8bb25bde0efcac789df38b415
Ayush Maurya <032001ayushmaurya@gmail.com>
2026-07-29 07:09:39 +0530

    Complete making class and using methods with params and learnt encapsulation_

--- excercises/python-basics/oop_demo.py
+++ excercises/python-basics/oop_demo.py
@@ -10,9 +10,23 @@ class Student:
     def __str__(self):
         return f"Student(name = {self.name}, age = {self.age})"
 
+    def have_birthday(self):
+         self.age += 1
+ 
+    def change_name(self,new_name):
+        self.name = new_name
+
 student1 = Student("Ayush", 25)
 student2 = Student("Rahul", 22)
 
 student1.introduce()
 student2.introduce()
+student1.have_birthday()
+print(student1)
+
+
+print(student1)
+
+student1.change_name("Amit")
+
 print(student1)
