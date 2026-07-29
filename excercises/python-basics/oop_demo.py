5a90689d0c18dcf4e8fe72bcbba9c216414ea545
Ayush Maurya <032001ayushmaurya@gmail.com>
2026-07-29 06:48:29 +0530

    Complete making class and using __str__

--- excercises/python-basics/oop_demo.py
+++ excercises/python-basics/oop_demo.py
@@ -1,19 +1,18 @@
 class Student:
-    def __init__(self,name,age): #constructor
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def introduce(self):
-        print(self)
+        print(f"Hi, I am {self.name} and I am {self.age} years old.")
+
+    def __str__(self):
+        return f"Student(name = {self.name}, age = {self.age})"
 
 student1 = Student("Ayush", 25)
 student2 = Student("Rahul", 22)
 
 student1.introduce()
 student2.introduce()
-
-print(student1.name)
-print(student1.age)
-
-print(student2.name)
-print(student2.age)
+print(student1)
